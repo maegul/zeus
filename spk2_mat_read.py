a602e1f2856c8c57bafd70cc8331af98cfb0d9e5
Errol Lloyd <errolkjlloyd@gmail.com>
2018-12-18 17:20:04 +1100

    New class for basic matlab conversion done manually using SON viewer matlab scripts

--- spk2_mat_read.py
+++ spk2_mat_read.py
@@ -8,6 +8,156 @@ import h5py
 import numpy as np
 
 
+class basicMatRead:
+	'''
+		Read basic manually generated matlab file format.
+
+		Generated using the SONVEIWER matlab libraries provided by CED
+
+
+
+	'''
+
+	def __init__(self, file_name, trace_channels=['Ch1'], marker_channel = 'Ch32',
+					base = 'output_struct'):
+
+		'''
+		Parameters
+		__________
+
+		file_name : str
+			file name or path to file containing HDF5 (.mat) data
+
+		trace_channels : iterable
+			Default - ['Ch1']
+			
+			Channel names that are to be extracted for easy access
+
+			Names must match the keys used in the HDF5 file.
+			PRESUMING data originates from spike2, CHannel names should simply 'Ch#' with
+			"#" being the number of the channel in spike2.
+
+			Ch32 is PRESUMED to be the marker channel
+
+		baseRE : str regex
+			RegEx for identifying the base key of the file. 
+			This is orindarily derived from the name of the spike2 file.
+			First group is used as the base
+
+		markerRe : str regex
+			As with baseRe, but for identifying the marker channel
+
+
+		Attributes
+		__________
+
+		Channels : list
+			Keys of all the channels in the data file
+
+		channel attrs : HDF5 objects
+			For each of the Trace Channels provided in the kwarg, there is
+			an attribute of the same name of either the class Trace or spikeTemp
+			This is for convenient access
+
+		MarkTimes : array (1d)
+			times of the markers from the marker channel
+
+		MarkCodes : array (1d)
+			Codes for each Marker Time.
+			Should encode stimulus type ... needs to be derived from lab data
+
+		TraceChannels : list
+			All Trace channels encoded as Trace types in same order as 
+			trace_channels (kwarg) (where possible) and _traceChannelKeys
+
+		SpikeTempChannels : list
+			As TraceChannels above
+
+
+		Methods
+		______
+
+		listChanData
+			Pretty Prints all channels and their attributes
+
+
+
+
+
+		'''
+
+		self.Dat = h5py.File(file_name)
+		self._baseName = base
+		self.DatBase = self.Dat[self._baseName]
+
+		self.Channels = self.listChannels(prnt=False)
+
+
+		self._markerChannelArg = marker_channel
+		self._trace_chan_args = trace_channels
+
+		self._marker_time_key = 'times'
+		self._marker_code_key = 'codes'
+
+		self._markChannel = self.DatBase[self._markerChannelArg]
+		self.MarkTimes = self._markChannel[self._marker_time_key].value.flatten()
+		self.MarkCodes = self._markChannel[self._marker_code_key].value.flatten()
+
+
+
+
+		self.TraceChannels = []
+		self.SpikeTempChannels = []
+
+		self._traceChannelNames = []
+		self._spikeTempChannelNames = []
+
+
+
+
+
+		# Adding specified channels as attributes and filtering by type
+		for tc in trace_channels:
+
+			assert tc in self.Channels, f'Channel {tc} is not available'
+
+			chan = self.DatBase[tc]
+
+
+
+			self.__dict__.update({tc: trace(chan, tc)})
+
+			self._traceChannelNames.append(tc)
+			self.TraceChannels.append(getattr(self, tc))
+
+
+
+
+
+
+
+
+	def listChannels(self, prnt=True):
+
+		# With basic format, everything is under basename
+		ks = list(self.Dat[self._baseName].keys())
+		if prnt:
+			pprint(ks)
+		else:
+			return ks
+
+	def listChanData(self, prnt=True):
+		self.ChanData = dict()
+
+		for ch in self.Channels:
+			self.ChanData.update({f'{ch}' : list(self.Dat[ch].keys())})
+
+		if prnt:
+			pprint(self.ChanData)
+		else:
+
+			return self.ChanData
+
 
 
 class spk2MatRead:
