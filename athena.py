4591cee7d7585d759bd6f87254eee2a01f765734
Errol Lloyd <errolkjlloyd@gmail.com>
2019-02-02 21:07:04 +1100

    proto type of athena

--- athena.py
+++ athena.py
@@ -4,6 +4,113 @@ from circ_stat import circ_stat as cs
 
 import cPickle
 import glob
+import copy
+
+
+import collections
+
+
+def update(d, u):
+	'''
+	For updating nested dictionaries without destroying elements
+
+	Parameters
+	__________
+
+	d: original dictionary
+	u: dictionary to update the original by, it will be inserted where appropriate
+
+	Returns
+	_______
+	d: original dictionary updated as is necessary
+
+	'''
+
+	for k, v in u.iteritems():
+
+		# if branch is dict
+		if isinstance(v, collections.Mapping):
+			# go down branches of d that match u ...
+			r = update(d.get(k, {}), v)
+			d[k] = r
+		else:
+			d[k] = u[k]
+	return d
+
+
+
+def meta_data_concat(spec, md):
+    '''
+    spec: list
+        specification of the meta data structure
+    md: list
+        actual meta data content to be used
+
+    Returns
+    _______
+    Concatenation of spec and md for use as keys in a nested dictionary
+    '''
+    return [param + '_' + str(md[param]) for param in spec]
+
+
+
+
+
+
+def metadata_to_dict(spec, md):
+    
+    combination = meta_data_concat(spec, md)
+
+    d = reduce(lambda x, y: {y:x}, reversed(combination+[dict()]))
+
+    return d
+
+
+
+
+def add_data_leaf(proj, spec, md, key=None, obj=None):
+    
+    dict_keys = meta_data_concat(spec, md)
+    
+    dict_item = proj
+
+    for i, k in enumerate(dict_keys):
+        
+        assert k in dict_item.keys(), '%s key is not in the dictionary' % k
+        
+        dict_item = dict_item[k]
+        
+        if i == len(dict_keys)-1:
+            dict_item.update({key: obj})
+        
+    return proj
+
+
+
+
+def get_data_leaf(proj, spec, md, ext_keys=None):
+    
+    dict_keys = meta_data_concat(spec, md)
+    
+    if ext_keys is not None:
+        if type(ext_keys) == str:
+            ext_keys = [ext_keys]
+            
+        dict_keys = dict_keys + ext_keys
+    
+    dict_item = proj
+
+    for i, k in enumerate(dict_keys):
+        
+        assert k in dict_item.keys(), '%s key is not in the dictionary' % k
+        
+        dict_item = dict_item[k]
+        
+        
+    return dict_item
+
+
+
 
 def descs_type_test(descs):
 
@@ -96,9 +203,13 @@ class athena:
         assert type(track) == int, 'Track should be integer'
         assert type(unit) == int, 'unit should be integer'
         assert type(run) == int, 'run should be integer'
+        assert type(desc) == str, 'description should be a string'
         
-        exp = exp.lower()
-        
+        exp = exp.upper()
+        desc = desc.lower()
+
+        self.__md_spec = ['experiment', 'track', 'unit', 'run']
+
         
         meta_data_output = dict(project_name = self.project_name,
                     question = self.question,
@@ -108,18 +219,69 @@ class athena:
                     run = run
                    )
     
+    	# Add description
         try:
             self.descs
             
             if desc is not None:
+
                 assert desc in self.descs, 'desc not prescribed'
                 
                 meta_data_output.update(desc=desc)
 
         except AttributeError:
             
-            print('There are no prescribed descriptions')
+            print('There are no prescribed descriptions ... make them!')
+
+        except AssertionError as e:
+            print('Your description is not prescribe')
+            print( repr(e))
+
+
+        try:
+            self.units = update(self.units, )
+
+
+        # use update to add unit metadata to master dictionary
+        # Add description to defaults?
+        # use add data to add pd cond_tuning to leaf
+        # Analysis functions on cond_tuning, using experiment type metadata from zeus? or just manual?
+
+        # use description to manage different experiments for each unit.
+        # cond_tuning stored in units.  Analysis done on that, and stored in unit.
+        # General, 'athena level' data is gathered from each experiment
+        # for each unit, all analysis results data is gathered to form a column in a dataframe
+        # each unit is a row, with a multi_index for the track and expeirment metadata also
+
+
+
+
             
+        meta_data_nested = dict(
+        	meta_data_output(self.__md_spec[0]) = dict(
+        		self.__md_spec[1] + str(meta_data_output(self.__md_spec[1])) = dict(
+        			self.__md_spec[2] + str(meta_data_output(self.__md_spec[2])) = dict(
+        				self.__md_spec[3] + str(meta_data_output(self.__md_spec[3])) = dict(
+        					desc = meta_data_output['desc']
+        					)
+        				)
+        			)
+        		)
+        	)
+
+
+        # Instantiate MetaData tree if it does not exist
+        try:
+        	assert type(self.units_meta_data) == dict, 'meta data structure should be a dictionary'
+
+    	except AttributeError:
+
+	        self.units_meta_data = dict()
+
+
+
+
+        self.units_meta_data = update(self.units_meta_data, meta_data_nested)
 
 
 
@@ -133,9 +295,13 @@ class athena:
 
         try:
             zeus_instance.cond_tuning
-            
-            print('yes')
 
+            meta_data = copy.deepcopy(zeus_instance.meta_data)
+
+           	# attach whol zeus instance to nested dictionary?
+
+
+            
         except AttributeError:
             
             print('zeus instance has no cond_tuning attribute ... analyse the data first!')
@@ -146,7 +312,7 @@ class athena:
         # adding where new, with notification
         # function for viewing the lists
             
-        return zeus_instance
+
 
 
 
